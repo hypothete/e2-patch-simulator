73feaf078870b54a7466ca3a3fcdcc4c7aca29a3
hypothete <erin@hypothete.com>
2023-01-01 14:06:12 -0800

    added share link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Graph from "./Graph";
 import Slider from "./Slider";
@@ -7,7 +7,34 @@ function App() {
   const [patches, setPatches] = useState(1);
   const [spread, setSpread] = useState(168);
   const [worn, setWorn] = useState(168);
+  const [shareState, setShareState] = useState(false);
   const period = 672; // month in hours
+
+  useEffect(() => {
+    const initParams = new URLSearchParams(window.location.search);
+    if (initParams.has("patches")) {
+      setPatches(Number(initParams.get("patches")));
+    }
+    if (initParams.has("spread")) {
+      setSpread(Number(initParams.get("spread")));
+    }
+    if (initParams.has("worn")) {
+      setWorn(Number(initParams.get("worn")));
+    }
+  }, []);
+
+  const copyShareLink = useCallback(async () => {
+    const newUrl = new URL(window.location.href);
+    newUrl.searchParams.set("patches", String(patches));
+    newUrl.searchParams.set("spread", String(spread));
+    newUrl.searchParams.set("worn", String(worn));
+    await navigator.clipboard.writeText(newUrl.toString());
+    setShareState(true);
+    setTimeout(() => {
+      setShareState(false);
+    }, 1000);
+  }, [patches, spread, worn]);
+
   return (
     <div className="App">
       <h1>0.1mg Estradiol Patch Simulator</h1>
@@ -40,11 +67,14 @@ function App() {
             setValue={setWorn}
           />
           <p>Patches used per month: {patches * Math.ceil(period / spread)}</p>
+          <button className="AppShare" onClick={copyShareLink}>
+            {shareState ? 'Copied!' : 'Copy Share Link'}
+          </button>
         </div>
       </div>
       <p>
-        Data for concentration curve is a weighted average of abdominal patch measurements taken from these
-        sources:{" "}
+        Data for concentration curve is a weighted average of abdominal patch
+        measurements taken from these sources:{" "}
         <a
           href="https://fda.report/DailyMed/e7e6da3b-8485-1382-61c9-e9b369018b98"
           target="_blank"
@@ -61,7 +91,13 @@ function App() {
         </a>
       </p>
       <p>
-        <a href="https://github.com/hypothete/e2-patch-simulator" target="_blank" rel="noreferrer">Code available on GitHub</a>
+        <a
+          href="https://github.com/hypothete/e2-patch-simulator"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Code available on GitHub
+        </a>
       </p>
     </div>
   );
